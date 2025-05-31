bbfdde3ea707292e6c385d0c1ba80689a15fc722
Your Name <your@email.com>
2025-05-31 21:35:42 +0530

    v

--- nostrogames/src/app/Games/page.tsx
+++ nostrogames/src/app/Games/page.tsx
@@ -45,7 +45,8 @@ export default function GameOnePage() {
   };
 
   return (
-    <div className="bg-gray-900 text-white">
+    // Make this wrapper at least the height of the viewport, and allow vertical scroll
+    <div className="bg-gray-900 text-white min-h-screen overflow-y-auto">
       <button
         onClick={() => router.back()}
         className="fixed top-10 left-14 p-3 rounded-full bg-gray-800 bg-opacity-50 backdrop-blur-md 
@@ -54,7 +55,7 @@ export default function GameOnePage() {
       >
         <FaArrowLeft size={20} />
       </button>
-      <div className="max-w-4xl min-h-screen mx-auto p-4 ">
+      <div className="max-w-4xl mx-auto p-4">
         <div className="bg-gray-800 bg-opacity-50 backdrop-blur-md rounded-xl p-6 shadow-lg">
           {!isGameRunning ? (
             <>
--- nostrogames/src/app/page.tsx
+++ nostrogames/src/app/page.tsx
@@ -16,7 +16,7 @@ import ColourfulText from "@/components/ui/colourful-text";
 export default function Home() {
   const router = useRouter();
   return (
-    <div className=" relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
+    <div className=" relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center ">
       <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
 
       {/* Main Content */}
